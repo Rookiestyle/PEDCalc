cbda3d4a94b4f3bf03bb19a17be371a5a5eb9982
Rookiestyle <rookiestyle@gmx.net>
2020-10-18 14:24:10 +0200

    Fix plgx creation
    Add .NET framework requirement to plgx

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.10")]
-[assembly: AssemblyFileVersion("1.10")]
+[assembly: AssemblyVersion("1.10.1")]
+[assembly: AssemblyFileVersion("1.10.1")]
 [assembly: Guid("3abc1af7-d517-4129-bbfe-d647983d48db")]
 
