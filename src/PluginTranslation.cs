f627419fdde226e26a3a8f8deefbc4704ae52e69
rookiestyle <rookiestyle@gmx.net>
2021-09-17 20:10:48 +0200

    Minor internal improvements

--- src/PluginTranslation.cs
+++ src/PluginTranslation.cs
@@ -143,9 +143,11 @@ This message will only be displayed once.";
 				}
 				catch (Exception ex)
 				{
-					lDebugStrings.Add("Error parsing file: " + ex.Message);
+					string sException = ex.Message;
+					if (ex.InnerException != null) sException += "\n" + ex.InnerException.Message;
+					lDebugStrings.Add("Error parsing file: " + sException);
 					LanguageCodeIso6391 = "en";
-					MessageBox.Show("Error parsing translation file\n" + ex.Message, PluginName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+					MessageBox.Show("Error parsing translation file\n\n" + sException, PluginName, MessageBoxButtons.OK, MessageBoxIcon.Error);
 					bError = true;
 					return LanguageCodeIso6391;
 				}
